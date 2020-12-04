03b36ab83a86d67666fafeded15c8cce03a435eb
adi0s-on <50942100+adi0s-on@users.noreply.github.com>
2020-12-04 14:27:03 +0100

    lil changes

---
+++ src/app/shared/models/user-details.ts
@@ -0,0 +1,8 @@
+export class UserDetails{
+    Id: number;
+    Height: number;
+    Weight: number;
+    Age: number;
+    BMR: number;
+    BMI: number;
+}
--- src/app/shared/models/user.model.ts
+++ src/app/shared/models/user.model.ts
@@ -1,10 +1,11 @@
 import { Diaries } from './diaries.model';
+import { UserDetails } from './user-details';
 
 export class User {
     Id: string;
     Name: string;
     Surname: string;
     City: string;
-    UserDetails: Object;
+    UserDetails: UserDetails;
     Diaries: Array<Diaries>;
 }
